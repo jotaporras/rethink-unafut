741973fab14de4ef76f34b12de80388023555850
Su tata <jimmy.solano@singularities.com>
2016-09-29 21:48:18 -0600

    Add stuff

---
+++ web/js/main.js
@@ -0,0 +1,35 @@
+console.log("hola");
+/*var vm = new Vue({
+  el: "#app",
+  data: {
+    messages: [],
+    input: ""
+  },
+  methods: {
+    post: function(e) {
+      socket.emit('chat message', this.input);
+      e.preventDefault();
+    }
+  }
+});
+
+socket.on('chat message', function(msg){
+  vm.messages.push(msg);
+});*/
+var socket = io();
+
+var example1 = new Vue({
+  el: '#app',
+  data: {
+    tweets: [
+      { user: "John",text: 'Foo' },
+      { user: "James",text: 'Bar' }
+    ]
+  },
+  methods: 
+});
+
+socket.on('chat message', function(msg){
+  vm.messages.push(msg);
+});
+
